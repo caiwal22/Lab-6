4ae8ec5585e2c3bec621e614717a417647b9a694
caiwal22 <159499059+caiwal22@users.noreply.github.com>
2024-03-08 08:22:44 -0700

    Add files via upload

---
+++ WallingCaitlin_Lab6.py
@@ -0,0 +1,112 @@
+# Caitlin Walling
+# UWYO COSC 1010
+# 3/7/2024
+# Lab 06
+# Lab Section: 40
+# Sources, people worked with, help given to: Autumn, Ryan (TA)
+# your
+# comments
+# here
+
+
+random_string = """
+jppamiqxegokaizvkyawwurhewtcxohryzptznyuedhhmawpic
+pkzwuiorngdfcsgqnlyifzyaivehpiyszykqprbcsobygzhadd
+yfddbulxmcnyvqhesmnybyuhxjqqmhdxwhcselasiayqhctnlw
+hakethjahqnvjdowhlyzosemxkbenestxgvgncmffkcxldcmkl
+itclmqdhrbdgzwtvdxwedcknbyaecvttjphtxubvhwvcvjqayy
+almxuxjbcmznnzekptfzbldsjwpvringlmalwufvlppeiendur
+dyophftqjkghhncwxoksqaqnpueudpygiytqgpcgjqsjbtbpzi
+vaeczmyicnednjjoxkpnjmpfbgyjnbfjlweqqppodfxfzzwkuf
+rldgryyhceuikimoavosuzuozthmatcgxcmkxnaxmsevkcumby
+spiajlbycvrluxdkfavxidzalxuixqkxiybhfuqhcvmrhzbzse
+idjwgwdwgfkyreozkyoxdvhixfejxjfgkkgobescboyfshiovu
+fxdyvfsnmjzsphgmtldlaoehofcspzujghcdcxzggvunpbtglr
+topplmkviuewwpoaplmbpgejmymxbyzzwbnujrlysszmxkjerb
+zpiewqvgopvhmmcgwcyvxvwhdvfgsrybcozhdtwujhdbxzznkc
+ergcqbetpgwrejuluqfxchlihunzbcdwboysjqenjxzbgqbycx
+dybxpyztjyxpkqfvxullzkedpkjjobhymfinpvprxejktyrpai
+ehjgwahpquzcmvclatdfcmattavoehnhnzveoxwnmnptxbvxto
+gpcobgzdhsjevhcohkltftmrqkosknkxeylhqxkkctbnusijgr
+uvecpbqmylqdaohkfaqbgeokyyipumjuaaayikdzyxfrpaieyo
+uxiosjwioebsjtslblfurgcodtyaggzovzfnnyjngawiwbbtqi
+kqqhnkwheolpqzasmsmbxqkeiqvogquobphewznfsnlkkizhca
+cbiyvxpmjxywqvzqtshfvnfbusphggexfqzepsrduvtovdsknl
+ztyuwugprkhbmktfvrenbmqgdjwnkeugtojrpqfmjhtrlcqcpq
+pwsguedzgvktpwbqkhkueymjtxbvzmdfjopzkygujrjdtogssg
+cxczryuqhhgjlpultkoffescpzyjrfqqabnhkfdnhkylpjamxk
+uxidjkqdrkxqjqjtflebvwhcvqjciykzhrvppvxhvpedgznwty
+kujglixooczrhxziasjxddfcghzlwrqcyiilpruhdfvitewxzg
+dzcvmvnoskchscgoqfsojfvahlwkrslzeirlblseplcmpmbmum
+ibrdamvqfstydtjopdkdcbnnmpifxckozyxzluhcqbqtpismog
+ulufaajxvuizvdzioxfvypxovptkibcrjvfidomejknuggfrtp
+kptwffersvqjknemkejsgspckwqisdcliuezhbeqpwgrjcqajl
+huobykkbujmyuuinbwdklqfhvakyozzsxghfyownjjwqtkxgkf
+ipdbjzxfogozstfsektujsvklrvecditiectuvtfibohmxxzna
+cpqzeoburtquuizhypugnkvuwbdxnraareqkofhfjobrpcsuxq
+nbafxlkuafbfsiuyrxdusqyasqyrwhdjrukgxdackumvairlgn
+fjhenwbrdghbevgqbybpwncclolgqyuhallbqtzdywbvlzwtil
+jctmsxjortnxvlbhuhkblppewjhqjzxrwgftlturxjuwfoaqpp
+sgfnxwxolkbrpdmpniitoljzaxabgtnelrmryetxqypwrjdyjc
+zipwbdpbazxpesmrcfuikeamtlsrgxrhzfytecenyydeemrhxj
+gmdruhillntvpadzbroyygydpmonwuakruvxbdrqhtrjvoqsin
+gjbarzvuqplmsmbwtqfghteoknbxmaokwlqqfoblmzsxczjzfj
+mzmawtarjdtgongqqufhhdjwcinhlxcsgoltjycxrkloqozxoi
+crlfmgflzzxgiiliqlksxyaydsohhahzxtsufzppftvgbpsdlx
+ertfmbothijzrrdvfrnsohnwulcxvcvxngvmznhazxrgdsugij
+fracotpirvqemsiuualpkpvtmtgchmowkmvoolrjfblrtwkmtr
+xhawucytgwlahddkhxxfublukkdldpovqokntydhzzrxiisdwu
+ujrkoewqoflyebogbwgdhriwkkoiofwtjlhxxtmzkklzbcmxhv
+lrslowamkcwolbcgfkfciegdwqskuazxnycqkkggzsowcmafay
+ibmkdwkqmdkjesqnjiqpijixbwjhenmsrrlpcseliiajlvcaac
+zkdenxczyooloczcaahnkehbwimvieedpdlqfafbqvxvfmvabd
+"""
+random_string = random_string.replace("\n","") #remove all newline characters
+print(len(random_string)) # Print out the size for reference 
+
+# Above is a string with 2500 characters.
+# Create a program that goes through and counts the occurrence of each character, excluding \n
+# Output: each letter and its corresponding occurrence in alphabetical order
+# Output which letter occurred the most 
+# Output which letter occurred the least 
+# Output what the percentage of the string each character is, again in alphabetical
+letters = {}
+for letter in sorted(random_string):
+    if letter not in letters:
+        letters[letter] = 1
+    else:
+        letters[letter] += 1
+print(letters)
+
+#Tips and trick:
+# You can iterate through strings like you would a list
+# All characters are lowercase 
+# Each letter will be PAIRED with its corresponding value 
+# That is to say, this is a great use of dictionaries
+    # You will  need to add the letter to the dictionary on first occurrence 
+    # Then increment its corresponding count 
+
+
+#Load all the elements into a dictionary
+#Will need to first declare a dictionary 
+
+# Output: each letter and its corresponding occurrence in alphabetical order
+
+print("*"*75)
+# Output which letter occurred the most 
+
+most_occurred = max(letters, key = letters.get)
+least_occurred = min(letters, key = letters.get)
+
+print(f"The letter that occurred the most is {most_occurred}")
+print("*"*75)
+# Output which letter occurred the least 
+print(f"The letter that occurred the least is {least_occurred}")
+print("*"*75)
+
+# Output what the percentage of the string each character is, again in alphabetical
+sums = len(random_string)
+for key in sorted(letters):
+    value = (letters[key])
+    percent = (value/sums)*100
+    print(f"The percentage of {key} is {percent}")
